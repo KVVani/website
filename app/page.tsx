c05567afd9f392f353502cd881ca3bf3dac2eb3f
KVVani <vijayavani.kolluru@postcodelottery.co.uk>
2024-11-08 15:18:17 +0000

    test

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <div>
       <h1 className=' m-8 text-6xl text-white text-center'> Welcome to Vani website </h1>
+      <h2 className=' m-8 text-6xl text-white text-center'> Know more about me </h1>
     </div>
   );
 }
